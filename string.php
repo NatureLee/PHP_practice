1123b962c45ef06404984c3b01e4246e73a4da1c
NatureLee <nature8340@gmail.com>
2019-05-31 01:04:15 +0900

    PHP basic
    string , variable

---
+++ string.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<body>
+  <h1>String & String Operator</h1>
+<?php
+echo "hello 'w'orld";
+ ?>
+ <h2>concatenation operator</h2>
+ <?php
+ echo "Hello "."world";
+  ?>
+  <h2>String length funtion</h2>
+  <?php
+  echo strlen("Hello world");
+   ?>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<body>
+  <h1>Variable</h1>
+  <?php
+  $name = "nature";
+  echo "Hi my name is $name. Nice to meet you.";
+  ?>
+  <p>
+  <?php
+  echo "Hi my name is ".$name.". Nice to meet you.";
+  ?>
+</body>
+</html>
