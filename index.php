ef66ab94ed2eaf34195eeb2e1e9cd25990648863
NatureLee <nature8340@gmail.com>
2019-06-01 01:06:43 +0900

    PHPpractice
    parameter, parameter ex

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
   <body>
-    <?php
-      echo date('Y-m-d H:i:s');
-    ?>
+    <h1>WEB</h1>
+    <ol>
+      <li><a href = "index.php?id=HTML">HTML</a></li>
+      <li><a href = "index.php?id=CSS">CSS</a></li>
+      <li><a href = "index.php?id=JavaScript">JabaScript</a></li>
+    </ol>
+    <h2>
+      <?php
+      echo $_GET['id'];
+      ?>
+    </h2>
   </body>
 </html>
