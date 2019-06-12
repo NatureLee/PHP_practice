1a2f82afccddacc9a164c9786d602c68c87c8b69
NatureLee <nature8340@gmail.com>
2019-06-13 02:24:16 +0900

    PHPpractice
    practice about Form..

--- index.php
+++ index.php
@@ -1,41 +1,55 @@
+<?php
+function print_title(){
+  if (isset($_GET['id'])) {
+    echo $_GET['id'];
+  }
+  else {
+    echo "Wellcome!";
+  }
+}
+function print_description(){
+  if (isset($_GET['id'])) {
+    echo file_get_contents("data/".$_GET['id']);
+  }
+  else {
+    echo " ";
+  }
+}
+function makelist(){
+        $list = scandir('./data');
+        $a = 0;
+        while ($a < count($list)) {
+          if ($list[$a] != '.' && $list[$a] != '..') {
+            echo "<li><a href = \"index.php?id=$list[$a]\">$list[$a]</a></li>\n";
+          }
+          $a= $a + 1;
+        }
+      }
+ ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>
+      <?php
+      print_title();
+      ?>
+    </title>
   </head>
   <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
       <?php
-       //list에 담아놓고 그걸 반복문으로 플어서 보여줌
-         $list = scandir('./data');
-         $a = 0;
-         while ($a < count($list)) {
-           if ($list[$a] != '.' && $list[$a] != '..') {
-             echo "<li><a href = \"index.php?id=$list[$a]\">$list[$a]</a></li>\n";
-           }
-           $a= $a + 1;
-         }
+       makelist();
         ?>
     </ol>
     <h2>
       <?php
-        if (isset($_GET['id'])) {
-          echo $_GET['id'];
-        }
-        else {
-          echo "Wellcome!";
-        }
-        ?>
-      </h2>
-      <?php
-      if (isset($_GET['id'])) {
-        echo file_get_contents("data/".$_GET['id']);
-      }
-      else {
-        echo " ";
-      }
-       ?>
+      print_title();
+      ?>
+    </h2>
+    <?php
+    print_description();
+     ?>
   </body>
 </html>
