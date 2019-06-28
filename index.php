97cd7a4b60c5ac98ff091e16f07b2c49adf49bd9
NatureLee <nature8340@gmail.com>
2019-06-29 04:59:52 +0900

    PHPpractice
    index.php , XSS.php

--- index.php
+++ index.php
@@ -1,48 +1,16 @@
 <?php
-function print_title(){
-  if (isset($_GET['id'])) {
-    echo $_GET['id'];
-  }
-  else {
-    echo "Wellcome!";
-  }
-}
-function print_description(){
-  if (isset($_GET['id'])) {
-    echo file_get_contents("data/".$_GET['id']);
-  }
-  else {
-    echo " ";
-  }
-}
-function makelist(){
-        $list = scandir('./data');
-        $a = 0;
-        while ($a < count($list)) {
-          if ($list[$a] != '.' && $list[$a] != '..') {
-            echo "<li><a href = \"index.php?id=$list[$a]\">$list[$a]</a></li>\n";
-          }
-          $a= $a + 1;
-        }
-      }
+require_once('lib/print.php');
+require_once('view/top.php');
  ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>
-      <?php
-      print_title();
-      ?>
-    </title>
-  </head>
-  <body>
-    <h1><a href="index.php">WEB</a></h1>
-    <ol>
-      <?php
-       makelist();
-        ?>
-    </ol>
+    <a href="create.php">create</a>
+    <?php if (isset($_GET['id'])) {
+    ?>
+    <a href="update.php?id=<?=$_GET['id'] ?>">update</a>
+    <form action="delete_process.php" method="post">
+      <input type="hidden" name="id" value="<?=$_GET['id'] ?>">
+      <input type="submit" value="delete">
+    </form>
+    <?php } ?>
     <h2>
       <?php
       print_title();
@@ -51,5 +19,6 @@ function makelist(){
     <?php
     print_description();
      ?>
-  </body>
-</html>
+<?php
+require('view/bottom.php');
+ ?>
